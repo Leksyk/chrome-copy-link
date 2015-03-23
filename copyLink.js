a2babc5a625e683c5f33a7bd474c0032151cc0c3
Volodya Shtenovych <me@leksyk.com>
2015-03-23 23:42:15 +0100

    - Ability to copy the page's link with a shortcut Ctrl+Shift+C ( Ctrl-Cmd-C for mac). - Showing notification when link is copied or an error happened.

--- copyLink.js
+++ copyLink.js
@@ -1,8 +1,59 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+// onClicked action gives us the tab without any permissions needed.
+chrome.browserAction.onClicked.addListener(copyTabLinkToClipboard);
+
+chrome.commands.onCommand.addListener(function(command) {
+  getActiveTab(copyTabLinkToClipboard);
+});
+
+function getActiveTab(callback) {
+  requestPermissions('tabs', function() {
+    chrome.tabs.query({
+        lastFocusedWindow: true,
+        active: true
+      }, function(tab) {
+        if (tab && tab.length > 0) {
+          callback(tab[0]);
+        } else {
+          showNotification('Error', 'Unable to get link: No active tab.');
+        }
+      });
+  });
+}
+
+function requestPermissions(permission, callback) {
+  chrome.permissions.request({
+    permissions: [permission]
+  }, function(granted) {
+    if (granted) {
+      callback();
+    } else {
+      if (permission != "notifications") {
+        showNotification('Error', 'Unable to get link: User rejected tabs permissions request.');
+      } else {
+        console.log('Unable to show the message: Notifications permissions rejected.');
+      }
+    }
+  });
+}
+
+function showNotification(title, text) {
+  requestPermissions('notifications', function() {
+    var opt = {
+      type: 'basic',
+      title: title,
+      message: text,
+      iconUrl: 'icon.png'
+    };
+    chrome.notifications.create("", opt, function() {});
+  });
+}
+
+function copyTabLinkToClipboard(tab) {
   var url = tab.url;
   var title = tab.title;
+  console.log('Copying to clipboard', url, title);
   copyLinkToClipboard(url, title);
-});
+}
 
 function copyLinkToClipboard(url, title) {
   var link = document.createElement('a');
@@ -13,6 +64,7 @@ function copyLinkToClipboard(url, title) {
     link.focus();
     document.execCommand('SelectAll');
     document.execCommand("Copy", false, null);
+    showNotification('Link is ready to be pasted', '"' + title + '" was copied to clipboard');
   } finally {
     link.remove();
   }
